b7703c187442a2c1092576fbca892432288f3b2e
Yrahi Quispe Reyna <yira1231@hotmail.com>
2025-09-18 12:06:49 -0500

    EliminarMarca

---
+++ src/main/java/com/apirest/gestiondeinventario_backend/marca/application/dto/request/EliminarMarcaRequestDto.java
@@ -0,0 +1,8 @@
+package com.apirest.gestiondeinventario_backend.marca.application.dto.request;
+
+import lombok.Data;
+
+@Data
+public class EliminarMarcaRequestDto {
+    private Integer idMarca;
+}
---
+++ src/main/java/com/apirest/gestiondeinventario_backend/marca/application/dto/response/EliminarMarcaResponseDto.java
@@ -0,0 +1,9 @@
+package com.apirest.gestiondeinventario_backend.marca.application.dto.response;
+
+import lombok.Data;
+
+@Data
+public class EliminarMarcaResponseDto {
+    private Integer idMarca;
+    private String nombre;
+}
